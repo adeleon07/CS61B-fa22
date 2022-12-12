463c0b8a4bbfc7c98f64081bee87f93d2ed8a843
Andre <andredeleon07@gmail.com>
2022-12-11 22:09:24 -0800

    added HellowNumbers Files

---
+++ HelloNumbers.java
@@ -0,0 +1,13 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int i = 1;
+        while (i < 10) {
+            System.out.print(x + " ");
+            x = x + i;
+            i ++;
+        }
+        System.out.println(x + " ");
+        System.out.println(x);
+    }
+}
